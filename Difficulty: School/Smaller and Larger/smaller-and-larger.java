584aedc1a14d385cd78db3a4269b32dd91d7d69e
Brijesh kumar <brijeshpal8878@gmail.com>
2024-07-08 09:53:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Smaller and Larger/smaller-and-larger.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans[] = new Solve().getMoreAndLess(arr, n, x);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        int max=0;
+        int min=0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]<=x)
+            {
+                min++;
+                
+            }
+            //else
+            if(arr[i]>=x)
+            {
+                max++;
+            }
+            
+        }
+         return new int[]{min,max};   
+        // code here
+    }
+}
