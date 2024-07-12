2d86614a5961f4e2e35341e1dffa54f3ed4109be
Brijesh kumar <brijeshpal8878@gmail.com>
2024-07-12 11:23:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/nPr/n-pr.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            Long n = Long.parseLong(a[0]);
+            Long r = Long.parseLong(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.nPr(n, r));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long nPr(long n, long r){
+        return factorial(n)/factorial(n-r);
+    }
+     static long factorial(long n){
+        long fact=1;
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+}
