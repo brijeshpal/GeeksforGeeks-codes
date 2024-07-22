e9d764540ca65a5efe0a2982b0e6fc2d6615dcdf
Brijesh kumar <brijeshpal8878@gmail.com>
2024-07-22 10:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Binary representation/binary-representation.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    string getBinaryRep(int N)
+    {
+        string str="";
+        for(int i=0; i<30; i++)
+        {
+            str=to_string(N%2)+str;
+            N/=2;
+        }
+        return str;
+        // Write Your Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+       
+        Solution ob;
+        string ans = ob.getBinaryRep(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
