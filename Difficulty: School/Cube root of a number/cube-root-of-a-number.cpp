9ddb08b5d5e789106e9b1555be8006d16a841d64
Brijesh kumar <brijeshpal8878@gmail.com>
2024-07-25 11:16:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Cube root of a number/cube-root-of-a-number.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cubeRoot(int N) {
+        double roo;
+        roo=cbrt(N);
+        return roo;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.cubeRoot(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
