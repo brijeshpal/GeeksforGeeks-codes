fc66cfd5c0af7a0837595524055ab275be8049b6
Brijesh kumar <brijeshpal8878@gmail.com>
2024-07-31 09:53:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Determine focal length of a spherical mirror/determine-focal-length-of-a-spherical-mirror.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int findFocalLength(float R, string type){
+	    if(type=="concave")
+	    {
+	      return floor(R/2);
+	    }
+	    else
+	    {
+	        return floor(-R/2);
+	    }
+	    //Code here
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		float n;
+		string type;
+		cin >> type >> n;
+		Solution ob;
+		int ans = ob.findFocalLength(n, type);
+		cout << ans <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
