00ad283ca95125cc92315df4a7305e68e93dae02
Brijesh kumar <brijeshpal8878@gmail.com>
2024-08-15 17:33:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Start Coding/start-coding.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void printHello() {
+        cout<<"Hello World";
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        Solution obj;
+        
+        obj.printHello();
+        
+    }
+}
+
+// } Driver Code Ends
