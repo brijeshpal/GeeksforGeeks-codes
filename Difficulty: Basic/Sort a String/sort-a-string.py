7cf5a58242f1de2b3af3e03c0b15ced696154cc0
Brijesh kumar <brijeshpal8878@gmail.com>
2024-08-17 09:40:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a String/sort-a-string.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+class Solution:
+    def sort(self, s): 
+        return "".join(sorted(list(s)))
+        #code here
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input().strip())
+    for _ in range(t):
+        s = input().strip()
+        obj = Solution()
+        ans = obj.sort(s)
+        print(ans)
+# } Driver Code Ends
